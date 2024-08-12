b4f597be5ca08d427b1e09b27f9e767fc7082715
Sergey <90842082+Qubicool@users.noreply.github.com>
2024-08-12 19:38:37 +0600

    Add genre of work and guild in registration

--- src/keyboards/user_keyboard.py
+++ src/keyboards/user_keyboard.py
@@ -33,8 +33,32 @@ def guild_keyboard():
     guild2 = KeyboardButton(text=guild_keyboard_button['guild2'])
     guild3 = KeyboardButton(text=guild_keyboard_button['guild3'])
 
-    guild_keyboard.row(guild1)
-    guild_keyboard.row(guild2)
-    guild_keyboard.row(guild3)
+    guild_keyboard.row(guild1, guild2, guild3)
 
     return guild_keyboard.as_markup(resize_keyboard=True)
+
+genres_of_work = { 
+    "Java": False, "Scala": False, "Python": False,
+    "C#": False, "C/C++": False, "Golang": False,
+    "Ruby": False, "PHP": False, "Node.js": False,
+    "Solidity": False, "Web Frontend": False,
+    "Vue": False, "Angular": False, "React": False,
+    "iOS": False, "Android": False, "Flutter": False,
+    "React Native": False,
+    }
+
+def genre_of_work_keyboard():
+    kb_builder = InlineKeyboardBuilder()
+    for language, selected in genres_of_work.items():
+        kb_builder.add(InlineKeyboardButton(
+            text=f"{'✅' if selected else ' '} {language}",
+            callback_data=language
+        ))
+    kb_builder.adjust(3)  # количество кнопок в строке
+    kb_builder.row(InlineKeyboardButton(text="Подтвердить", callback_data="confirm"))
+    return kb_builder.as_markup()
+
+def skip_keyboard():
+    kb_builder = InlineKeyboardBuilder()
+    kb_builder.row(InlineKeyboardButton(text="Пропустить", callback_data="skip"))
+    return kb_builder.as_markup()
